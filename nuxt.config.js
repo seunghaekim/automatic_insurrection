e9cf6b03ca5199f9f9a8958627519ead3ac5a8e1
seunghaekim <aka.seunghaekim@gmail.com>
2020-02-02 22:02:00 +0900

    FIX some configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,15 @@
 export default {
-  srcDir: 'src/'
+  srcDir: 'src/',
+  buildModules: [
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-78070436-7'
+    }]
+  ],
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { hid: 'description', name: 'description', content: 'javascript port of automatic_insurrection' },
+      { title: 'Automatic Insurrection js'}
+    ]
+  }
 }
